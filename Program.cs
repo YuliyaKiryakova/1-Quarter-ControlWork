5e15ae9fb65432967f84e73040fee17dcc95fb52
YuliyaKiryakova <yulk_krya@mail.ru>
2023-07-26 10:46:12 +0300

    Create Code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+
+string[] CreateInputArray(int size)
+{
+    return new string[size];
+}
+
+void FillArray(string[] arrayIn)
+{
+    for (int i = 0; i < arrayIn.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        arrayIn[i] = Console.ReadLine()!;
+    }
+}
+
+void CreateOutputArray(string[] arrayIn, string[] arrayOut)
+{
+    int index = 0;
+    for (int i = 0; i < arrayIn.Length; i++)
+    {
+        if (arrayIn[i].Length <= 3)
+        {
+            arrayOut[index] = arrayIn[i];
+            index++;
+        }
+    }
+}
+
+void PrintArray(string[] arrayOut)
+{
+    for (int i = 0; i < arrayOut.Length; i++)
+    {
+        Console.Write($"{arrayOut[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int length = InputNum("Введите размер массива: ");
+string[] myArray1 = CreateInputArray(length);
+string[] myArray2 = CreateInputArray(length);
+FillArray(myArray1);
+CreateOutputArray(myArray1, myArray2);
+PrintArray(myArray2);
