fc6ddf2be5d3bbdedf988a88d3e8af594bd8281d
YuliyaKiryakova <yulk_krya@mail.ru>
2023-07-27 10:29:34 +0300

    Upgraded 2 program.cs

--- Program.cs
+++ Program.cs
@@ -18,27 +18,54 @@ void FillArray(string[] arrayIn)
     }
 }
 
-void CreateOutputArray(string[] arrayIn, string[] arrayOut)
+int CountElementsInArrayOut(string[] arrayIn)
 {
+    int count = 0;
     for (int i = 0; i < arrayIn.Length; i++)
     {
         if (arrayIn[i].Length <= 3)
-            arrayOut[i] = arrayIn[i];
+            count++;
+    }
+    return count;
+}
+
+void CreateOutputArray(string[] arrayIn, string[] arrayOut)
+{
+    int index = 0;
+    for (int i = 0; i < arrayIn.Length; i++)
+    {
+        if (arrayIn[i].Length <= 3)
+        {
+            arrayOut[index] = arrayIn[i];
+            index++;
+        }
     }
 }
 
 void PrintArray(string[] arrayOut)
 {
-    for (int i = 0; i < arrayOut.Length; i++)
+    Console.Write(" [");
+    for (int i = 0; i < arrayOut.Length - 1; i++)
     {
-        Console.Write($"{arrayOut[i]} ");
+        Console.Write($"{arrayOut[i]}, ");
     }
-    Console.WriteLine();
+    Console.Write($"{arrayOut[arrayOut.Length - 1]}");
+    Console.WriteLine("] ");
 }
 
 int length = InputNum("Введите размер массива: ");
 string[] myArray1 = CreateInputArray(length);
-string[] myArray2 = CreateInputArray(length);
 FillArray(myArray1);
-CreateOutputArray(myArray1, myArray2);
-PrintArray(myArray2);
+
+int count = CountElementsInArrayOut(myArray1);
+
+if (count == 0)
+{
+    Console.Write("Элементов, удовлетворяющих условию, в массиве нет..");
+}
+else
+{
+    string[] myArray2 = CreateInputArray(count);
+    CreateOutputArray(myArray1, myArray2);
+    PrintArray(myArray2);
+}
